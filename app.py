39ab196e082e1268f86a76c9904602bbc323ba4c
uccsuren <surendar.aakkamgroups@gmail.com>
2025-08-01 22:09:16 +0530

    Third commit

--- app.py
+++ app.py
@@ -7,5 +7,9 @@ app = Flask(__name__)
 def hello():
     return "Hello from Python on Render!"
 
+@app.route("/welcome")
+def hai():
+    return "welcome"
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=10000)
